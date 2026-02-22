c2f3ec53731ed7e392593221897827e207ffacfb
UlisesAramis <odiseoaramis@gmail>
2026-02-21 22:30:59 -0600

    fix: add hover

--- src/components/ui/ButtonBee.tsx
+++ src/components/ui/ButtonBee.tsx
@@ -9,18 +9,20 @@ const ButtonBee = ({ children, className = "", ...props }: ButtonBeeProps) => {
   return (
     <button
       className={`
-        bg-black text-white px-4 py-2 rounded mt-4 
-        flex items-center justify-center gap-2
-        transition-all duration-150
-        hover:opacity-90
-        active:scale-95 active:translate-y-0.5
-        shadow-md active:shadow-sm
-        ${className}
-      `}
+      group
+      bg-black hover:bg-neon-sky text-white hover:text-black px-4 py-2 rounded mt-4 
+      flex items-center justify-center gap-2
+      transition-all duration-150
+      active:scale-95 active:translate-y-0.5
+      shadow-md active:shadow-sm
+      ${className}
+    `}
       {...props}
     >
       <span>{children}</span>
-      <span className="text-[#F3E600] text-lg">{">"}</span>
+      <span className="text-[#F3E600] group-hover:text-black text-lg">
+        {">"}
+      </span>
     </button>
   );
 };
