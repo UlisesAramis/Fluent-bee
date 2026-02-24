0922569efeff17d8cf33a1a12a60eb1cf8ebe783
UlisesAramis <odiseoaramis@gmail.com>
2026-02-23 21:20:20 -0600

    Feat: add Instructions component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,13 @@
 import SpeakEnglish from "./SpeakEnglish";
 import FreeTrail from "./FreeTrail";
+import Instructions from "./Instructions";
 
 const HomeBee = () => {
   return (
     <main>
       <SpeakEnglish />
       <FreeTrail />
+      <Instructions />
     </main>
   );
 };
