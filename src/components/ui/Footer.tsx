a2c41fe1c120026e7013a2f304dae70f33b617a1
UlisesAramis <odiseoaramis@gmail.com>
2026-04-06 21:20:43 -0600

    fix: delate href

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,22 +1,27 @@
-import Link from "next/link";
 import { FaSquareXTwitter } from "react-icons/fa6";
 
 const links = [
-  { href: "/privacy", label: "Privacy Policy" },
-  { href: "/terms", label: "Terms of Service" },
-  { href: "/cookies", label: "Cookies Policy" },
+  { label: "Privacy Policy" },
+  { label: "Terms of Service" },
+  { label: "Cookies Policy" },
 ];
 
 const Footer = () => {
   return (
-    <footer className="h-40 bg-solar-neon flex flex-col justify-center items-center gap-y-5">
-      <FaSquareXTwitter className="text-black w-10 h-10" />
+    <footer className="bg-solar-neon flex flex-col justify-center items-center gap-6 py-10">
+      <FaSquareXTwitter className="text-black w-10 h-10 transition-transform duration-200 hover:scale-110 active:scale-95" />
 
-      <div className="text-black w-5/6 flex flex-col md:flex-row justify-evenly items-center pb-20">
-        {links.map(({ href, label }) => (
-          <Link key={href} href={href} className="text-2xl">
+      <div className="text-black w-5/6 flex flex-col md:flex-row justify-evenly items-center gap-4">
+        {links.map(({ label }) => (
+          <button
+            key={label}
+            className="
+            cursor-pointer
+            text-2xl
+            "
+          >
             {label}
-          </Link>
+          </button>
         ))}
       </div>
     </footer>
