6bc2b9dd394099ef44b9503ebde176531de3a3b3
UlisesAramis <odiseoaramis@gmail.com>
2026-04-06 22:21:12 -0600

    fix: github pages base path

--- docs/_next/static/GrHEqtmgICe5eHVxFTG8z/_buildManifest.js
+++ docs/_next/static/GrHEqtmgICe5eHVxFTG8z/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Fluent-bee//_next/:path+",
+        "destination": "/Fluent-bee/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "export",
+
+  basePath: "/Fluent-bee",
+  assetPrefix: "/Fluent-bee/",
 };
 
 export default nextConfig;
