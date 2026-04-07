922b99d537b4c64b5fa6a21fbaa8652651a207f6
UlisesAramis <odiseoaramis@gmail.com>
2026-04-06 22:52:51 -0600

    fix: chage nextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  output: "export",
-
-  basePath: "/Fluent-bee",
-  assetPrefix: "/Fluent-bee/",
 };
 
 export default nextConfig;
