0db04186c6e3539b4180800ee4a2f7a6b2115b36
UlisesAramis <odiseoaramis@gmail.com>
2026-04-06 23:24:19 -0600

    deploy: github pages ready

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  output: "export",
+
+  basePath: "/Fluent-bee",
+  assetPrefix: "/Fluent-bee/",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
